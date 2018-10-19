09d0c139b020edf8f8333ae23870db195ba0abae
Josh Williams <vmizzle@gmail.com>
2018-10-19 14:17:39 -0500

    Absorb memorex-ui

--- lib/memorex_web/controllers/playlist_controller.ex
+++
@@ -1,8 +0,0 @@
-defmodule MemorexWeb.PlaylistController do
-  use MemorexWeb, :controller
-
-  def show(conn, %{"id" => id}) do
-    {:ok, %Memorex.Playlist{} = playlist} = Memorex.Playlist.fetch(id)
-    render(conn, "show.json", playlist: playlist)
-  end
-end
--- lib/memorex_web/views/playlist_view.ex
+++
@@ -1,7 +0,0 @@
-defmodule MemorexWeb.PlaylistView do
-  use MemorexWeb, :view
-
-  def render("show.json", %{playlist: playlist}) do
-    %{playlist: playlist}
-  end
-end
